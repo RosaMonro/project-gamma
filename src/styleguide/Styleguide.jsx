2383626e9a3ae52be0dbc68f5df4fef59f2e08a1
Rosa <=>
2023-05-03 20:53:08 +0200

    empiezo tipografia

--- src/styleguide/Styleguide.jsx
+++ src/styleguide/Styleguide.jsx
@@ -22,13 +22,24 @@ export default function Styleguide () {
             </div>
         </section>
 
-        <section className="sg__headers">
-            <h1>Soy un encabezado h1</h1>
-            <h2>Soy un encabezado h2</h2>
-            <h3>Soy un encabezado h3</h3>
-            <h4>Soy un encabezado h4</h4>
-            <h5>Soy un encabezado h5</h5>
-            <h6>Soy un encabezado h6</h6>
+        <section className="sg__typo">
+            <div className="sg__typo--headers">
+                <h1>Soy un encabezado h1</h1>
+                <h2>Soy un encabezado h2</h2>
+                <h3>Soy un encabezado h3</h3>
+                <h4>Soy un encabezado h4</h4>
+                <h5>Soy un encabezado h5</h5>
+                <h6>Soy un encabezado h6</h6>
+            </div>
+
+            <div className="sg__typo--paragraf">
+                <h1>Soy un encabezado h1</h1>
+                <h2>Soy un encabezado h2</h2>
+                <h3>Soy un encabezado h3</h3>
+                <h4>Soy un encabezado h4</h4>
+                <h5>Soy un encabezado h5</h5>
+                <h6>Soy un encabezado h6</h6>
+            </div>
         </section>
 
     </>
