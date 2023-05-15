da0ea1b8d7ecf47d2fa090401787ff8082415b74
Rosa <=>
2023-05-15 16:10:22 +0200

    Cambios clase día 10

--- src/components/Btn/Btn.jsx
+++ src/components/Btn/Btn.jsx
@@ -2,17 +2,16 @@ import './Btn.scss'
 
 export default function Button () {
     return <>
+        <div>
+            <button className="btn--primary">Botón</button>
+            <button className="btn--secondary">Botón</button>
+            <button className="btn--cta">Botón</button>
+        </div>
 
-    <div>
-        <button className="btn--primary">Botón</button>
-        <button className="btn--secondary">Botón</button>
-        <button className="btn--cta">Botón</button>
-    </div>
-
-    <div>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-    </div>
+        <div>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+        </div>
     </>
 }
