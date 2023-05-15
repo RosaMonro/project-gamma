441c56bd05a82c83fe0151040070a769c02758c0
Rosa <=>
2023-05-15 16:50:12 +0200

    Botones y enlaces

--- src/components/Btn/Btn.jsx
+++ src/components/Btn/Btn.jsx
@@ -3,15 +3,15 @@ import './Btn.scss'
 export default function Button () {
     return <>
         <div>
-            <button className="btn--primary">Botón</button>
-            <button className="btn--secondary">Botón</button>
-            <button className="btn--cta">Botón</button>
+            <button className="btn  btn--primary">Botón</button>
+            <button className="btn  btn--secondary">Botón</button>
+            <button className="btn  btn--cta">Botón</button>
         </div>
 
         <div>
-            <a href=""></a>
-            <a href=""></a>
-            <a href=""></a>
+            <a className="link  link--primary" href="">Soy un enlace</a>
+            <a className="link  link--secondary" href="">Soy un enlace</a>
+            <a className="link  link--cta" href="">Soy un enlace</a>
         </div>
     </>
 }
