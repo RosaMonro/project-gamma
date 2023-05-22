4fad5d8339de9483f51f90b8b222bc29c616adae
RosaMonro <rosamaria.monro@gmail.com>
2023-05-23 01:21:20 +0200

    corregido error en carpeta components

--- src/components/Btn/Btn.jsx
+++ src/components/Btn/Btn.jsx
@@ -1,9 +1,10 @@
+
 import './Btn.scss'
 
 export default function MyButton (props) {
     if (props.isButton == 'true') {
         return <button className={"btn  btn--" + props.variant}>{props.text}</button>
     }else {
-        return <a className={"link  link--" + props.variant} href="">Soy un enlace</a>
+        return <a className={"link  link--" + props.variant} href="">{props.text}</a>
     }
 }
