35b835e066be9e805c347db15d2d0f3172435553
RosaMonro <rosamaria.monro@gmail.com>
2023-05-23 02:22:28 +0200

    componentización header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import './scss/global.scss'
 import Styleguide from './styleguide/Styleguide'
-import MyButton from './components/MyButton/MyButton'
+import { CardHeader } from './components/CardHeader/CardHeader'
 
 
 
@@ -14,31 +14,8 @@ function App() {
           <Route path="/styleguide" element={<Styleguide />} />
         </Routes>
 
-      <header className="header  header-bg">
+        <CardHeader name='Rosa Montero'  role='Frontend'></CardHeader>
 
-        <div className="header__container">
-
-            <div className="header__info">
-                <p className="h1  font-weight-bold  font-style-italic">Rosa Montero</p>
-                <h1 className="font-weight-light  font-style-italic">Frontend</h1>
-            </div>
-
-            <div className="header__button">
-                <MyButton variant="secondary" text="CONTÁCTAME" isButton="true"></MyButton>
-                <MyButton  variant="cta" text="MI TRABAJO" isButton="true"></MyButton>
-            </div>
-
-            <div className="header__cta">
-              <p className="font-weight-light  font-size-20-xs">¡Puedes leer más <MyButton  variant="secondary" text="sobre mí" isButton="false"></MyButton>!</p>
-            </div>
-
-        </div>
-
-      </header>
-
-      <main>
-        
-      </main>
     </>
   )
 }
--- src/components/CardHeader/CardHeader.jsx
+++ src/components/CardHeader/CardHeader.jsx
@@ -1,1 +1,29 @@
+import MyButton from '../MyButton/MyButton'
 import './CardHeader.scss'
+
+export function CardHeader(props) {
+
+    return (
+        <header className="header  header-bg">
+
+        <div className="header__container">
+
+            <div className="header__info">
+                <p className="h1  font-weight-bold  font-style-italic">{props.name}</p>
+                <h1 className="font-weight-light  font-style-italic">{props.role}</h1>
+            </div>
+
+            <div className="header__button">
+                <MyButton variant="secondary" text="CONTÁCTAME" isButton="true"></MyButton>
+                <MyButton  variant="cta" text="MI TRABAJO" isButton="true"></MyButton>
+            </div>
+
+            <div className="header__cta">
+              <p className="font-weight-light  font-size-20-xs">¡Puedes leer más <MyButton  variant="secondary" text="sobre mí" isButton="false"></MyButton>!</p>
+            </div>
+
+        </div>
+
+      </header>
+    )
+}
