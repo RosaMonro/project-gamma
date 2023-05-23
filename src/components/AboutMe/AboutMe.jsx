e8b647ccdfeefebce59d70145ff884a1438277bb
RosaMonro <rosamaria.monro@gmail.com>
2023-05-23 17:31:05 +0200

    main title, main line

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -3,10 +3,16 @@ import './AboutMe.scss'
 export function AboutMe(props) {
     return(
 
-        <div className='main-title'>
-            <h3 className="font-style-italic  font-weight-semibold">Quién soy y qué hago</h3>
+        <div className='main'>
+            
+            <div className='main-title'>
+                <h3 className="font-style-italic  font-weight-semibold">Quién soy y qué hago</h3>
+            </div>
+
+            <div>
+                <hr class="horizontal-line"></hr>
+            </div>
+
         </div>
-
-
     )
 }
