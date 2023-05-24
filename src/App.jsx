6a226d7676cc3d98b0f686f017bb49f1bed1de27
RosaMonro <rosamaria.monro@gmail.com>
2023-05-24 12:23:10 +0200

    modificación copy del about

--- src/App.jsx
+++ src/App.jsx
@@ -31,32 +31,34 @@ function App() {
                 <div className="text-about">
                     <h4 className="h6  font-weight-light  color-fucsia">Soy frontend-UI</h4>
                     <p className="font-size-16-xxs  font-weight-light">Realicé un posgrado en UX/UI, 
-                    desarrollo frontend y marketing digital donde aprendí HTML y CSS con el uso de 
-                    herramientas como sass y metodología BEM. También me inicié en el desarrollo 
-                    con JavaScript y el manejo de React. Asimismo, tengo experiencia en el uso de 
-                    Github, Visual Studio Code, Figma y WordPress.</p>
+                    desarrollo frontend y marketing digital. Aprendí HTML y CSS también con el uso de 
+                    herramientas como Sass y de la metodología BEM. Además me inicié en el desarrollo 
+                    con JavaScript y el manejo de React y tengo experiencia en el uso de Github, 
+                    Visual Studio Code, Figma y WordPress.</p>
 
                     <h4 className="h6  font-weight-light  color-fucsia">También soy psicóloga</h4>
                     <p className="font-size-16-xxs  font-weight-light">Licenciada en psicología y especializada 
-                    en neuropsicología. Tengo una amplia experiencia en administración e interpretación de 
-                    pruebas neuropsicológicas, diseño de intervención basada en la evidencia y elaboración 
+                    en neuropsicología, poseo una amplia experiencia en entrevistas y administración e interpretación de 
+                    pruebas neuropsicológicas, analísis de conducta, diseño de intervención basada en la evidencia y elaboración 
                     de objetivos SMART así como en el trabajo en equipo interdisciplinar y en comunicación 
                     y asesoramiento familiar. </p>
                 </div>
 
                 <div className="text-about">
                     <h4 className="h6  font-weight-light  color-fucsia">Aportando valor...</h4>
-                    <p className="font-size-16-xxs  font-weight-light">Aunque la psicología y el desarrollo 
-                    frontend parecen dos áreas muy diferentes, ambas requieren habilidades técnicas y 
-                    analíticas, una meticulosa atención al detalle, entrenamiento en la resolución de 
+                    <p className="font-size-16-xxs  font-weight-light">La psicología, así como 
+                    el diseño y desarrollo de interfaces requieren de habilidades tanto técnicas como 
+                    analíticas para saber interpretar patrones de conducta del usuario y 
+                    así poder implementarlas en el desarrollo de las mismas. Asimismo, es importante una meticulosa atención al detalle, entrenamiento en la resolución de 
                     problemas y una sólida capacidad de comunicación efectiva para trabajar en equipo.</p>
+                    
                     
                     <h4 className="h6  font-weight-light  color-fucsia">...y algo más.</h4>
                     <p className="font-size-16-xxs  font-weight-light">Soy una persona sociable y sensible 
                     con mucho sentido del humor. Me gusta la fotografía y me apasiona el baile. Tengo 
                     por objetivos seguir aprendiendo a expresarme a través de la danza, explorando el 
                     mundo que me rodea y creciendo como persona y como profesional en el ámbito del 
-                    diseño, la experiencia de usuario y desarrollo web.</p>
+                    diseño, la experiencia de usuario y el desarrollo web.</p>
                 </div>
 
             </div>
