a320bd332e4faf03ef65a9abdbb703cb684be4aa
RosaMonro <rosamaria.monro@gmail.com>
2023-05-29 13:05:55 +0200

    corrección color en error404

--- src/views/Error404.jsx
+++ src/views/Error404.jsx
@@ -4,7 +4,7 @@ import '../scss/global.scss'
 export default function Error404() {
     return (
         <div className="bg-error404">
-            <p className="h1 error-text__giant color-fucsia-darken  font-weight-semibold">¡¡Yepa!!</p>
+            <p className="h1 error-text__giant color-fucsia  font-weight-semibold">¡¡Yepa!!</p>
             <p className="error error-404 font-weight-bold">404</p>
             <MyButton variant="cta" text="Vuelve conmigo" isButton="true" url="/Home"></MyButton>
 
