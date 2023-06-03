8608f2e35f5d3f9d24d6071f0d65430a960d848e
RosaMonro <rosamaria.monro@gmail.com>
2023-06-03 21:38:13 +0200

    fetch

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,9 +1,25 @@
 import CardProject from '../components/CardProjects/CardProject'
 import MyLink from '../components/MyLinks/MyLink'
 import '../scss/global.scss'
+import React, { useEffect, useState } from 'react';
 
 
 export default function Projects() {
+
+    const [jsonData, setJsonData] = useState([]);
+
+  useEffect(() => { //nos permite hacer la petición HTTP
+    fetch('../../projects.json')
+    //usamons then() para encadenar acciones a una promesa resuelta exitosamente
+      .then(response => response.json()) //para obtener los datos en formato json
+      .then(data => {
+        setJsonData(data); //los datos se pasan a la función data y se establecen en el estado jsonData
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []); //con un array vacío, se ejecuta una sola vez cuando se monte el componente. 
+
     return (
         <>
 
@@ -23,9 +39,21 @@ export default function Projects() {
         </header>
 
         <main className="color-bg-white-darken">
-            <CardProject></CardProject>
-            <CardProject></CardProject>
-            <CardProject></CardProject>
+
+            {jsonData.map(item => (
+            <CardProject
+                key={item.id}
+                image={item.image}
+                alt={item.title}
+                title={item.title}
+                subtitle={item.subtitle}
+                date={item.date}
+                abstractDescription={item.abstractDescription}
+                description={item.description.split('\n').map((paragraph, index) => (
+                    <p key={index}>{paragraph}</p>
+                  ))}
+            />
+            ))}
 
             <div className="link-home">
                 <MyLink  variant="primary" text="Volver a al inicio" isButton="false" url="/"></MyLink>
