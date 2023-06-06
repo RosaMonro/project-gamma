06ed5aae4fe9b6cae77f794a44dcea35e3d6b89a
RosaMonro <rosamaria.monro@gmail.com>
2023-06-06 23:09:11 +0200

    Framer Motion: useScroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,24 @@ import Home from './views/Home'
 import Projects from './views/Projects'
 import Error404 from './views/Error404'
 import Footer from '../src/components/Footer/Footer'
+import { motion, useScroll, useSpring } from "framer-motion";
 
 
 function App() {
   const location = useLocation();
   const is404Route = location.pathname === '/Error404';
 
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
   return (
     <>
+      <motion.div className="progress-bar" style={{ scaleX }} />
+
       <Routes>
         <Route path="/Styleguide" element={<Styleguide />} />
         <Route path="/" element={<Home />} />
